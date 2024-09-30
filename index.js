4e8430a97f2348917a3b4dca526501f25ef67ec7
Lee01-bit <jaleellee19@gmail.com>
2024-09-30 15:54:29 +0200

    updated

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
-const { json } = require("stream/consumers");
-let lastId = 0
 const PORT = 3000;
 const fileName = 'fileData.json';
+
+
 
 
 //Two functions that will be reading and writing items into my Json file
@@ -14,7 +14,7 @@ const readItems =() => {
 };
 
 const writeItems = (dataItems) => {
-    fs.writeFileSync(fileName, JSON.stringify(dataItems,null, 2));
+    fs.writeFileSync(fileName, JSON.stringify(dataItems, null, 2));
  };
 
 const server = http.createServer((req, res) =>{
@@ -22,14 +22,16 @@ const server = http.createServer((req, res) =>{
     const method = req.method;
 
 // to GET information 
-    if (urlParsed.pathname === "/products" && method === "GET"){
+    if (urlParsed.pathname === "/products"  && method === "GET"){
         const data = readItems();
-        res.statusCode = 200
-        res.setHeader =('Content-Type', 'application/json' )
+        // res.statusCode = 200
+        res.writeHead(200, {'Content-Type': 'application/json' })
         res.end(JSON.stringify(data))
+       
+
     } else if (urlParsed.pathname === "/products" && method === "POST"){
         // POST : Adding new Items to the list 
-        let body = " ";
+        let body = "";
 
         req.on("data", chunk =>{
             body += chunk.toString();
@@ -37,24 +39,71 @@ const server = http.createServer((req, res) =>{
 
         req.on("end", () =>{
             const itemNew = JSON.parse(body);
-            const data = readItems()
-            itemNew.id = (++lastId);;
+            const data = readItems();
+            itemNew.id = data.length ? data[data.length - 1].id + 1:1
             data.push(itemNew);
-            writeItems(data)
-            res.setHeader =('Content-Type', 'application/json' )
-            res.end(JSON.stringify(data))
-            console.log(itemNew)
+            writeItems(data);
+            res.writeHead(200, {'Content-Type': 'application/json' });
+            res.end(JSON.stringify(data));
+            console.log("New Item Has been Added!");
+            
+       });
+     } else if  (urlParsed.pathname.startsWith("/products/") && method === "PUT") {
+         // Extracting the ID from the URL path
+        const itemId = urlParsed.pathname.split('/')[2];
+         console.log(itemId);
+        
+     let body = "";
+    
+         req.on("data", chunk => {
+             body += chunk.toString();
         });
-    } 
+    
+         req.on("end", () => {
+            try {
+                const updatedItem = JSON.parse(body);
+             const data = readItems();
+                
+                // Find the index of the item with the matching ID
+                 const index = data.findIndex(item => parseInt(item.id) === itemId); // Assuming ID is a string
+    
+                if (index === -1) {
+                    res.writeHead(404, { "Content-Type": "application/json" });
+                    res.end(JSON.stringify({ error: "Item Not Found" }));
+                     return;
+               }
+            
+                 // Update the item in the array
+               data[index] = { ...data[index], ...updatedItem };
+    
+    
+                res.writeHead(200, { "Content-Type": "application/json" });
+                 res.end(JSON.stringify(data[index])); // Return the updated item
+            } catch (err) {
+                 res.writeHead(400, { "Content-Type": "application/json" });
+                 res.end(JSON.stringify({ error: "Invalid JSON" }));
+             }
+       });
+     }
+    
 
 
 
 
 
-    else {
+
+
+
+
+
+
+
+
+
+ else {
         // 404 Not Found
         res.writeHead(404);
-        res.end('This is the Welcome page');
+        res.end('Content Not Found');
     }
 });
 
